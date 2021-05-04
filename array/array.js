edf469ce6a5bf8e43cf1d25540f8ea143f012b44
VICTOR <rutokoechvictor@gmail.com>
2021-05-04 19:10:54 +0300

    vbn

---
+++ array/array.js
@@ -0,0 +1,6 @@
+let fruits = ['mango','orange','apple'];
+const farmTools = ['jembe','panga','fork','hammer'];
+fruits [0] = 'lemon';
+console.log(fruits);
+farmTools [3] = 'machete';
+console.log(farmtools);
